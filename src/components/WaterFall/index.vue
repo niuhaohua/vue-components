9edee4017181e4cb2ffd5f3ba63081cd2b6c386a
niuhaohua <niuhaohua@ccx.com>
2019-05-16 10:54:11 +0800

    first commit

---
+++ src/components/WaterFall/index.vue
@@ -0,0 +1,104 @@
+<template>
+  <div ref="waterFallBox">
+    <div
+      ref="waterFallBoxItem"
+      v-for="column in columns"
+      class="waterFall-item"
+      :style="{width:itemWidth+'px',marginLeft: column == 1?'0':'20px'}"
+    >
+      <div class="column-item" v-for="item in itemList[column-1]">{{item.name+'：'+item.value}}</div>
+    </div>
+  </div>
+</template>
+<script>
+import { setTimeout, setInterval, clearInterval } from 'timers';
+export default {
+  name: 'WaterFallComponent',
+  props: {
+    width: {
+      type: Number,
+      required: 300
+    },
+    list:{
+      type:Array,
+      required:[]
+    }
+  },
+
+  data() {
+    return {
+      itemWidth: this.width,
+      columns: 3,
+      columnLengthMin: 0,
+      itemList: [],
+      data:this.list
+    }
+  },
+  created() {
+
+  },
+  watch: {
+    list: {
+      handler(newName, oldName) {
+        this.data = this.list
+        this.computeColumnLength()
+      },
+      immediate: true,
+      deep: true
+    },
+  },
+  mounted() {
+    this.computeColumn(this.itemWidth)
+  },
+  methods: {
+    //计算列数
+    computeColumn(val) {
+      let containerWidth = this.$refs.waterFallBox.clientWidth
+      this.columns = Math.floor((containerWidth - 20) / (val + 20))
+      if (this.columns <= 0) {
+        this.columns = 1
+        this.itemWidth = containerWidth
+      }
+
+      this.$nextTick(() => {
+        for (let i = 0; i < this.columns; i++) {
+          this.itemList[i] = []
+        }
+        this.computeColumnLength()
+      })
+    },
+    //计算哪列最短
+    computeColumnLength() {
+      let itemArray = this.$refs.waterFallBoxItem
+      let lengthMin = 0
+      for (let i in itemArray) {
+        if (itemArray[i].clientHeight < itemArray[lengthMin].clientHeight) {
+          lengthMin = i
+        }
+      }
+      this.renderData(lengthMin)
+    },
+    //拼接新数据
+    renderData(col) {
+      for(let key of this.data){
+        this.itemList.push([])
+        this.itemList.pop()
+        this.itemList[col].push(key)
+      }
+    }
+  }
+
+}
+</script>
+<style scoped>
+.waterFall-item {
+  display: inline-block;
+  /* height: 1000px; */
+  /* background: #eea9b8; */
+  vertical-align: top;
+}
+.column-item{
+  margin-bottom: 20px;
+  /* background:#F5DEB3; */
+}
+</style>
---
+++ src/views/waterfall/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    我是页面
+    <button @click="add">add</button>
+    <waterfall :width="200" :list="list"/>
+  </div>
+</template>
+<script>
+import Waterfall from '@/components/WaterFall/index'
+export default {
+  name: 'WaterFall',
+  components: {
+    Waterfall,
+  },
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+
+  },
+  methods: {
+    add() {
+      this.list = []
+      let count = Math.floor(Math.random() * 10)
+      console.log(count)
+      let array = []
+      for (let i = 0; i < count; i++) {
+        this.list.push({ name: 'hahahahahia', value: count * Math.random() })
+      }
+    }
+  }
+
+}
+</script>
